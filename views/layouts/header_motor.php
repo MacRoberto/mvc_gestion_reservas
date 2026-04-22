94820d97e5aa3c9627d16ef591756cc99ca33d50
MacRoberto <roberto-may-c@outlook.es>
2026-04-21 20:07:54 -0500

    ajuste-style-claseLogo

--- views/layouts/header_motor.php
+++ views/layouts/header_motor.php
@@ -15,7 +15,7 @@
         <div class="d-flex justify-content-between align-items-center mb-3">
             
             <a href="index.php">
-                <img src="assets/img/logo-may.png" alt="ExperienciasMay" class="pt-logo">
+                <img src="assets/img/logo-may.png" alt="ExperienciasMay" class="em-logo">
             </a>
 
             <div class="d-flex align-items-center gap-4 top-header">
